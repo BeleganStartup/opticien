cef2832e7d78a90f500d458c5a251eca4b368f9d
BeleganStartup <okba2fb@gmail.com>
2021-07-04 00:59:40 +0100

    dev contact using modal

--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -1,21 +1,50 @@
+// React
+import { useState, useEffect } from 'react'
 // React Bootstrap
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import NavDropdown from 'react-bootstrap/NavDropdown'
 import Container from 'react-bootstrap/Container'
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
 // Icons
 import { VscAccount, VscHome } from "react-icons/vsc"
-import { FaFileInvoiceDollar } from "react-icons/fa"
+import { FaFileInvoiceDollar, FaLinkedin } from "react-icons/fa"
 import { GiStabbedNote } from "react-icons/gi"
 import { RiAccountCircleFill } from "react-icons/ri"
 import { IoSettings } from "react-icons/io5"
+import { SiStackoverflow, SiInstagram } from "react-icons/si" 
+import { AiOutlineGithub } from "react-icons/ai"
 
 
 const NavigationBar = () => {
     const navDropdownTitle = (<><RiAccountCircleFill/> Account</>);
 
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    
     return (
         <Navbar bg="dark" variant="dark" expand="lg">
+
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>Contactez moi</Modal.Title>
+            </Modal.Header>
+            <Modal.Body className="text-center">
+                <div><a target="_blank" className="text-dark text-decoration-none" href="https://www.instagram.com/prince2m3/"><SiInstagram/> Instagram</a></div>
+                <div><a target="_blank" className="text-dark text-decoration-none" href="https://www.linkedin.com/in/boularaouiokba/"><FaLinkedin/> Linkedin</a></div>
+                <div><a target="_blank" className="text-dark text-decoration-none" href="https://github.com/BeleganStartup"><AiOutlineGithub/> Github</a></div>
+                <div><a target="_blank" className="text-dark text-decoration-none" href="https://stackoverflow.com/users/10273196/b-okba"><SiStackoverflow/> Stackoverflow</a></div>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                Close
+                </Button>
+            </Modal.Footer>
+        </Modal>
+
         <Container>
             <Navbar.Brand href="#home">Opticien V1.0</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -31,7 +60,7 @@ const NavigationBar = () => {
                     <NavDropdown.Item href="#action/3.1">Connexion</NavDropdown.Item>
                     <NavDropdown.Item href="#action/3.1">Se déconnecter</NavDropdown.Item>
                     <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Contacter le développeur</NavDropdown.Item>
+                    <NavDropdown.Item onClick={handleShow}>Contacter le développeur</NavDropdown.Item>
                 </NavDropdown>
                 <Nav.Link href="#link"><IoSettings /> Paramètre</Nav.Link>
             </Nav>
